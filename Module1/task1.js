547b6473e8fc97f29a55e42034b650e08ae89b73
MALLAVALLI APPARAQ <54364675+14761A04E9@users.noreply.github.com>
2020-06-15 20:33:34 +0530

    task 2 completed

---
+++ Module1/task1.js
@@ -0,0 +1,14 @@
+
+function add(){
+
+  let  parent =  document.getElementById("myDiv");
+
+  var iDiv = document.createElement('div');
+  //iDiv. = "block";
+  iDiv.id = 'block';
+  
+  
+  parent.appendChild(iDiv);
+
+
+}
