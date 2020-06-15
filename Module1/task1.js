30d139f9e6b7133ea7bb7e25bb73af545ac075f9
MALLAVALLI APPARAQ <54364675+14761A04E9@users.noreply.github.com>
2020-06-15 20:54:29 +0530

    task 2 completed

--- Module1/task1.js
+++ Module1/task1.js
@@ -5,10 +5,27 @@ function add(){
 
   var iDiv = document.createElement('div');
   //iDiv. = "block";
-  iDiv.id = 'block';
+  iDiv.className = 'block';
   
   
   parent.appendChild(iDiv);
 
 
 }
+
+
+function setRandomColor(e) {
+  e.target.style.backgroundColor=getRandomColor();
+}
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+  color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+ }
+ 
+
+ document.getElementsByClassName('block').addEventListener("mouseover",setRandomColor);
